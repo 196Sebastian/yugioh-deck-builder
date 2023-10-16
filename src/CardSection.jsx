23ef39ff36a9b21c272067f203f0978d37ced4cf
196Sebastian <correa.sebas6@gmail.com>
2023-10-16 23:09:13 +0900

    getting the images to display on the screen

--- src/CardSection.jsx
+++ src/CardSection.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import Select from "react-select/creatable";
 import SearchOptions from "./searchInfo";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import "./card-section-style.css";
 
 const CardSection = () => {
   const [attributeInfo, setAttribute] = useState([]);
@@ -11,6 +15,7 @@ const CardSection = () => {
     );
     const data = await response.json();
     setAttribute(data.data);
+    console.log(data.data);
   };
 
   return (
@@ -21,6 +26,35 @@ const CardSection = () => {
           getAttributeData(apiEndPoint.value);
         }}
       />
+
+      {attributeInfo.length !== 0 ? (
+        <>
+          <div>{console.log(attributeInfo[0].card_images[0].image_url)}</div>
+          <ImageList
+            sx={{
+              width: 580,
+              height: 500,
+            }}
+            gap={30}
+          >
+            {attributeInfo.map((item) => (
+              <ImageListItem key={item.id}>
+                <img
+                  srcSet={item.card_images[0].image_url}
+                  src={item.card_images[0].image_url}
+                  alt={item.name}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.name}
+                  subtitle={<span>by: {item.name}</span>}
+                  position="below"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </>
+      ) : null}
     </div>
   );
 };
