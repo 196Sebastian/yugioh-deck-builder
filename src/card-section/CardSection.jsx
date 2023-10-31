c886f0581beb0da44f06dbfe629cced2b9b7d4f6
196Sebastian <correa.sebas6@gmail.com>
2023-10-31 13:20:25 +0900

    added logic to separate type when clicked

--- src/card-section/CardSection.jsx
+++ src/card-section/CardSection.jsx
@@ -3,6 +3,43 @@ import "../card-section/card-section.css";
 
 const CardSection = (props) => {
   const [deck, setDeck] = useState([]);
+  const [extraDeck, setExtraDeck] = useState([]);
+
+  const setExtraDeckFun = (item) => {
+    setExtraDeck([
+      ...extraDeck,
+      {
+        id: item.id,
+        image_url: item.card_images[0].image_url,
+      },
+    ]);
+  };
+
+  const separateDeck = (item) => {
+    if (item.frameType === "link") {
+      setExtraDeckFun(item);
+    } else if (item.frameType === "xyz") {
+      setExtraDeckFun(item);
+    } else if (item.frameType === "synchro") {
+      setExtraDeckFun(item);
+    } else if (item.frameType === "fusion") {
+      setExtraDeckFun(item);
+    } else if (item.frameType === "fusion_pendulum") {
+      setExtraDeckFun(item);
+    } else if (item.frameType === "synchro_pendulum") {
+      setExtraDeckFun(item);
+    } else if (item.frameType === "xyz_pendulum") {
+      setExtraDeckFun(item);
+    } else {
+      setDeck([
+        ...deck,
+        {
+          id: item.id,
+          image_url: item.card_images[0].image_url,
+        },
+      ]);
+    }
+  };
 
   return (
     <>
@@ -20,13 +57,7 @@ const CardSection = (props) => {
                 src={item.card_images[0].image_url}
                 alt={item.name}
                 onClick={() => {
-                  setDeck([
-                    ...deck,
-                    {
-                      id: item.id,
-                      image_url: item.card_images[0].image_url,
-                    },
-                  ]);
+                  separateDeck(item);
                 }}
               />
             ))}
@@ -35,14 +66,29 @@ const CardSection = (props) => {
       </div>
 
       <div className="display">
-        {deck.map((item) => (
-          <img
-            className="image"
-            key={item.id}
-            src={item.image_url}
-            alt={item.name}
-          />
-        ))}
+        <div>
+          {deck.map((item, index) => (
+            <img
+              className="image"
+              id={item.id}
+              key={index}
+              src={item.image_url}
+              alt={item.name}
+            />
+          ))}
+        </div>
+
+        <div>
+          {extraDeck.map((item, index) => (
+            <img
+              className="image"
+              id={item.id}
+              key={index}
+              src={item.image_url}
+              alt={item.name}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
